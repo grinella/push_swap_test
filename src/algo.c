354fb6be12f421afd091683fefc17f0045144360
Gabriele Rinella <grinella@e3r5p1.42roma.it>
2023-06-23 13:02:17 +0200

    23_giu_2023a

--- src/algo.c
+++ src/algo.c
@@ -6,11 +6,35 @@
 /*   By: grinella <grinella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:15:07 by grinella          #+#    #+#             */
-/*   Updated: 2023/06/23 12:35:07 by grinella         ###   ########.fr       */
+/*   Updated: 2023/06/23 12:55:44 by grinella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/push_swap.h"
+
+void	tre_numeri(t_stack **stack)
+{
+	t_stack	*a;
+	t_stack	*b;
+	t_stack	*c;
+
+	a = (*stack);
+	b = a->next;
+	c = b->next;
+	if (!(a->value < b->value && b->value < c->value))
+	{
+		if ((a->value < b->value && b->value > c->value && c->value > a->value)
+			|| (a->value > b->value && b->value < c->value
+				&& c->value > a->value)
+			|| (a->value > b->value && b->value > c->value))
+			s_(stack, 1);
+		if (a->value < b->value && b->value > c->value && c->value < a->value)
+			rr_(stack, 1);
+		else if (a->value > b->value && b->value < c->value
+			&& c->value < a->value)
+			r_(stack, 1);
+	}
+}
 
 // void	tre_numeri(t_stack **stack)
 // {
@@ -46,27 +70,3 @@
 // 	}
 // 	return ;
 // }
-
-void	tre_numeri(t_stack **stack)
-{
-	t_stack	*a;
-	t_stack	*b;
-	t_stack	*c;
-
-	a = (*stack);
-	b = a->next;
-	c = b->next;
-	if (!(a->value < b->value && b->value < c->value))
-	{
-		if ((a->value < b->value && b->value > c->value && c->value > a->value)
-			|| (a->value > b->value && b->value < c->value
-				&& c->value > a->value)
-			|| (a->value > b->value && b->value > c->value))
-			s_(stack, 1);
-		if (a->value < b->value && b->value > c->value && c->value < a->value)
-			rr_(stack, 1);
-		else if (a->value > b->value && b->value < c->value
-			&& c->value < a->value)
-			r_(stack, 1);
-	}
-}
